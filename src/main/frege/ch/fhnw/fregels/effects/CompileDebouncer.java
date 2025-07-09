3cc7fccc4f282b6b74871f1adfc296d5b43f7c64
tobias.wyss <tobias.wyss@unic.com>
2025-07-09 13:33:24 +0200

    Move packages from ch.fhnw.lsp to ch.fhnw.fregels

---
+++ src/main/frege/ch/fhnw/fregels/effects/CompileDebouncer.java
@@ -0,0 +1,25 @@
+package ch.fhnw.fregels.effects;
+
+import java.util.concurrent.*;
+
+public class CompileDebouncer {
+    private static final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private static long delayMillis = 500;
+    private static ScheduledFuture<?> scheduledFuture;
+
+    public CompileDebouncer(long delayMillis) {
+        this.delayMillis = delayMillis;
+    }
+
+    public synchronized void trigger(Runnable action) {
+        if (scheduledFuture != null && !scheduledFuture.isDone()) {
+            scheduledFuture.cancel(false);
+        }
+        scheduledFuture = scheduler.schedule(action, delayMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public void shutdown() {
+        scheduler.shutdown();
+    }
+}
+

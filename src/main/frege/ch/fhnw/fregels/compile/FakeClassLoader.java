1268a8054eb47f93b63468ced7dbbe6a673b5a51
tobias.wyss <tobias.wyss@unic.com>
2025-07-11 17:32:53 +0200

    Add DidChange tests & simplify PublishDiagnostics

---
+++ src/main/frege/ch/fhnw/fregels/compile/FakeClassLoader.java
@@ -0,0 +1,25 @@
+package ch.fhnw.fregels.compile;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class FakeClassLoader extends URLClassLoader {
+
+    public FakeClassLoader() {
+        super(new URL[0]);
+    }
+
+    /**
+     * Overrides loadClass to do nothing and always return Object.class.
+     */
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return Object.class;
+    }
+
+
+    /** Inspired by the way java.Lang.Runnable is built */
+    public static java.net.URLClassLoader classLoader() {
+        return new FakeClassLoader();
+    }
+}
